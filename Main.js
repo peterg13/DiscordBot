c8c93ed6d18bbf670d8a012b0be05a1e76ee1496
peter <petergustafson19@yahoo.com>
2018-02-25 15:38:22 -0800

    added plagueis

--- Main.js
+++ Main.js
@@ -57,6 +57,9 @@ client.on('message', message => {
             thunderfuryCommand(message);
             message.delete(7000);
         }
+        if(msg.includes("darth") || msg.includes("plagueis")){
+            darthPlageuis(message);
+        }
     }
     else{
         if (msgsToDelete > 0){
@@ -79,6 +82,10 @@ var thunderfuryCommand = function(msg){
         msg.channel.send('I guess I was just hearing things.');
     }, 8000);
 };
+
+var darthPlageuis = function(msg){
+    msg.channel.send("Did you ever hear the Tragedy of Darth Plagueis the wise? I thought not. It's not a story the Jedi would tell you. It's a Sith legend. Darth Plagueis was a Dark Lord of the Sith, so powerful and so wise he could use the Force to influence the midichlorians to create life... He had such a knowledge of the dark side that he could even keep the ones he cared about from dying. The dark side of the Force is a pathway to many abilities some consider to be unnatural. He became so powerful... the only thing he was afraid of was losing his power, which eventually, of course, he did. Unfortunately, he taught his apprentice everything he knew, then his apprentice killed him in his sleep. It's ironic he could save others from death, but not himself.");
+}
 
 //logs the client in
 client.login(auth.token);
