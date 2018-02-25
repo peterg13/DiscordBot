3da5da78ce0cc8da6d08a84a107499f698e6b67f
peterg13 <petergustafson19@yahoo.com>
2018-02-25 00:19:26 -0800

    DID SOMEBODY SAY THUNDERFURY?

--- Main.js
+++ Main.js
@@ -13,14 +13,43 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
+//the countr for messages to delete
+var msgsToDelete = 0;
+
 //what happens when a message is received
 client.on('message', msg => {
-    if (msg.content === 'ping') {
-        msg.channel.send('pong');
+    //if the message is from this bot it skips unless there are messages to delete
+    if (msg.author.username != "Community Guild Bot"){
+        //if some says ping bot responds with pong
+        if (msg.content == 'ping') {
+            msg.channel.send('pong');
+        }
+        if (msg.content.includes("thunderfury") || msg.content.includes("Thunderfury")){
+            thunderfuryCommand(msg);
+            msg.delete(7000);
+        }
+    }
+    else{
+        if (msgsToDelete > 0){
+            msg.delete(7000);
+            msgsToDelete -= 1;
+        }
     }
 });
 
+var thunderfuryCommand = function(msg){
+    msg.channel.send('Did sombody say...');
+    msg.channel.send("https://imgur.com/fkgAyXv.jpg");
+    msg.channel.send("http://media-azeroth.cursecdn.com/attachments/22/258/634950700589428230.jpg");
+    msg.channel.send("https://static.fjcdn.com/pictures/Blessed+blade+of+the+windseeker+did+someone+say+orangethunderfury+blessed_a679c6_5315363.png");
+    msg.channel.send("https://blizzardwatch.com/wp-content/uploads/2016/03/tf-murloc.jpg");
 
+    msgsToDelete = 5;
+
+    setTimeout(function(){
+        msg.channel.send('I guess I was just hearing things.');
+    }, 8000);
+};
 
 //logs the client in
 client.login(auth.token);
